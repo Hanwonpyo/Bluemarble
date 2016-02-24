3c23dc85615d23f42b4fb22a6a72f2f350924fa0
Hanwonpyo <olarya@naver.com>
2016-02-24 18:35:06 +0900

    first commit

---
+++ bluemarbel/src/bluemarbel/main.java
@@ -0,0 +1,12 @@
+package bluemarbel;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//roomGUI room=new roomGUI();
+		mapGUI map=new mapGUI();
+		map.makingGUI();
+		//login.temp();
+	}
+}
